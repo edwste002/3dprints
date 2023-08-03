c4cdb7a34c8b9730e4cf9aaaff50803fbb9f3ead
eee <e@f.com>
2023-08-03 02:26:30 -0400

    widget
    working widget...just needs faces and edges.

--- cvt/2023-07-27-blender-cvt-spiral.py
+++ cvt/2023-07-27-blender-cvt-spiral.py
@@ -1,5 +1,14 @@
 import math
 
+import io
+
+def print_to_string(*args, **kwargs):
+    output = io.StringIO()
+    print(*args, file=output, **kwargs)
+    contents = output.getvalue()
+    output.close()
+    return contents
+
 radiusOnTop = 20
 radiusOnBottom = 40
 
@@ -12,69 +21,78 @@ numberOfPointsOnTread = 3
 heightOfTread = 0.5
 widthOfTread = 2
 
-arcDistancePerTreadAndRecess = (2 * radiusOnTop * math.pi) / numberOfTreads
-arcPercentageForTopOfTread = widthOfTread / arcDistancePerTreadAndRecess
-arcPercentageForRecessOfTread = 1 - (widthOfTread / arcDistancePerTreadAndRecess )
-
-#print(arcDistancePerTreadAndRecess)
-#print(arcPercentageForTopOfTread)
-
-#numberOfXYDefinitionPoints = 20
 numberOfZDefinitionPoints = 10
 
-#numberOfZDefinitionPoints = 1
+topRadiusArcDistancePerTread = widthOfTread
 
-#def positionOfXYOfTreadAtZ(z=0,treadNumber=0,isTread=False):
-#    return 
+topRadiusArcDistancePerRecess = ((2 * radiusOnTop * math.pi) - (numberOfTreads * widthOfTread)) / (numberOfTreads)
 
-#figure out mesh position...see if you can do it in layers...if so do that    
+#calculate recess based on tread length
+topRadiusArcDistancePerTreadAndRecess = (2 * radiusOnTop * math.pi) / numberOfTreads
+arcPercentageForTopRadiusTreadAndRecess = (2 * math.pi) / numberOfTreads
+arcPercentageForTopRadiusTread = widthOfTread / topRadiusArcDistancePerTreadAndRecess
+arcPercentageForTopRecessOfTread = arcPercentageForTopRadiusTreadAndRecess - arcPercentageForTopRadiusTread
+
+
+radiusSlopePerNumberOfZDefintionPoints = (radiusOnBottom - radiusOnTop) / numberOfZDefinitionPoints
+
+#print('topRadiusArcDistancePerTreadAndRecess,topRadiusArcDistancePerTread,topRadiusArcDistancePerRecess')
+#print(topRadiusArcDistancePerTreadAndRecess,topRadiusArcDistancePerTread,topRadiusArcDistancePerRecess)
+
+#print('arcPercentageForTopRadiusTreadAndRecess,arcPercentageForTopRadiusTread,arcPercentageForTopRecessOfTread')
+#print(arcPercentageForTopRadiusTreadAndRecess,arcPercentageForTopRadiusTread,arcPercentageForTopRecessOfTread)
+
 
 cvt = []
 
-radiusSlopePerNumberOfZDefintionPoints = (radiusOnBottom - radiusOnTop)
+def getArcRatio(arcLengthForPreviousRadius, newArcRadius):
+    getCurrentRatio = arcLengthForPreviousRadius / ( 2 * newArcRadius * math.pi)
+    return getCurrentRatio
 
-def getArcRatio(arcDistancePerTreadAndRecess, currentRadius):
-    getCurrentCircumferencePercentage = arcDistancePerTreadAndRecess / (2 * currentRadius * math.pi)
-    #print('arc length')
-    #print(getCurrentCircumferencePercentage)
-    return getCurrentCircumferencePercentage
-
-def makePointsAlongArc(currentRadius,numberOfPoints,thetaStart,thetaEnd):
+def makePointsAlongArc(currentRadius,numberOfPoints,thetaStart,thetaEnd,currentZ):
     l=[]
-    thetaStep = (thetaEnd - thetaStart) / numberOfPoints
+    #fix wrap around issue
+    thetaStep = abs(thetaEnd - thetaStart) / (numberOfPoints)
     #print('thetaStart,thetaStep,thetaEnd')
     #print(thetaStart,thetaStep,thetaEnd)
     for i in range(numberOfPoints):
+        #print(i,'i cos sin', math.cos(thetaStart + thetaStep*i), math.sin(thetaStart + thetaStep*i))
+        #print('cos sin')
+        #print(math.cos(thetaStart + thetaStep*i))
+        #print(math.sin(thetaStart + thetaStep*i))
         ll=[currentRadius * math.cos(thetaStart + thetaStep*i),
               currentRadius * math.sin(thetaStart + thetaStep*i),
-              (z/numberOfZDefinitionPoints)*lengthOfGear]
+              currentZ]
         l.append(ll)
+    ll=[currentRadius * math.cos(thetaStart + thetaStep*numberOfPoints),
+              currentRadius * math.sin(thetaStart + thetaStep*numberOfPoints),
+              currentZ]
+    l.append(ll)
     #print('----')
     #print(l)
     return l
 
 
 cvt = []
-for z in range(numberOfZDefinitionPoints):
-    currentRadius = treadRecessHeight = radiusOnTop + (z/numberOfZDefinitionPoints)*radiusSlopePerNumberOfZDefintionPoints
+for z in range(numberOfZDefinitionPoints+1):
+    #print('----z-----')
+    currentRadius = treadRecessHeight = radiusOnTop + z*radiusSlopePerNumberOfZDefintionPoints
     treadHeight = treadRecessHeight + heightOfTread
-    getCurrentCircumferencePercentage = getArcRatio(arcDistancePerTreadAndRecess, currentRadius)
-    getCurrentCircumferencePercentageOnTread = getArcRatio(arcDistancePerTreadAndRecess, treadHeight)
+    getCurrentCircumferenceTreadAndRecessPercentage = getArcRatio(topRadiusArcDistancePerTreadAndRecess, currentRadius)
+    getCurrentCircumferenceTreadPercentage = getArcRatio(topRadiusArcDistancePerTread, currentRadius)
+    getCurrentCircumferenceTreadRecessPercentage = getArcRatio(topRadiusArcDistancePerRecess, currentRadius)
+
+    #print(getCurrentCircumferenceTreadAndRecessPercentage,getCurrentCircumferenceTreadPercentage,getCurrentCircumferenceTreadRecessPercentage)
+    #print('getCurrentCircumferenceTreadAndRecessPercentage,getCurrentCircumferenceTreadPercentage,getCurrentCircumferenceTreadRecessPercentage')
     for t in range(numberOfTreads):
         #print('-----------------'+str(t)+'----------------------------'+str(getCurrentCircumferencePercentageOnTread)+' ' + str(treadHeight))
-        cvt = cvt + makePointsAlongArc(currentRadius,
-                                       numberOfPointsOnRecessedArc,
-                                       (2*math.pi)*getCurrentCircumferencePercentage*t,
-                                       (2*math.pi)*getCurrentCircumferencePercentage*t + (2*math.pi)*(arcPercentageForRecessOfTread))
-
-        distance =  (((2*math.pi)*getCurrentCircumferencePercentageOnTread*(t+1)) - ((2*math.pi)*getCurrentCircumferencePercentageOnTread*t + (2*math.pi)*getCurrentCircumferencePercentageOnTread))/numberOfPointsOnTread
-        #print('non-recessed')
-        #print(distance)
-        cvt = cvt + makePointsAlongArc(treadHeight,
-                                       numberOfPointsOnTread,
-                                       (2*math.pi)*getCurrentCircumferencePercentage*t + (2*math.pi)*(arcPercentageForRecessOfTread),
-                                       (2*math.pi)*getCurrentCircumferencePercentage*(t+2)
-                                        )
+        cvt = cvt + makePointsAlongArc(treadHeight,numberOfPointsOnTread,(2*math.pi) * getCurrentCircumferenceTreadAndRecessPercentage*t,(2*math.pi) * getCurrentCircumferenceTreadAndRecessPercentage*t + (2*math.pi) * getCurrentCircumferenceTreadPercentage,currentRadius)
+        cvt = cvt + makePointsAlongArc(currentRadius,numberOfPointsOnRecessedArc,(2*math.pi) * getCurrentCircumferenceTreadAndRecessPercentage*t + (2*math.pi) * getCurrentCircumferenceTreadPercentage,(2*math.pi) * getCurrentCircumferenceTreadAndRecessPercentage*(t+1),currentRadius)
+    #add other side to this
+    if not z==0:
+        cvt = cvt + makePointsAlongArc(currentRadius,numberOfPointsOnTread + numberOfPointsOnRecessedArc, (2*math.pi) * getCurrentCircumferenceTreadAndRecessPercentage*(numberOfTreads+1),0,currentRadius)
 
 
-print(cvt)
+with open('output.txt','w') as f:
+    s = print_to_string(cvt)
+    f.write(s)
